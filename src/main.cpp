a4128f2c50d57dcaaa1fdca83a27846bda126dfe
adev <long.devillard@gmail.com>
2025-03-31 00:33:26 +0100

    fixed examples main and main_lsl

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "clvHd.hpp"
-// #include <iomanip>
+#include <iomanip>
 // #include <iostream>
 // #include <stdio.h>
 // #include <stdlib.h>
@@ -28,70 +28,39 @@ main(int argc, char *argv[])
 
     try
     {
-        std::cout << "CleverHand Serial Interface:" << std::endl;
+        std::cout << "CleverHand Serial Interfacee:" << std::endl;
         ClvHd::Device device(3);
+        std::cout << "Opening serial port: " << port << std::endl;
         device.initSerial(port.c_str());
 
         ClvHd::EMG_ADS1293Pack emg_pack(&device, 3);
         emg_pack.setup();
 
-        //         //open the serial connection between the computer and the controller board
-        //         device.controller.open_connection(port.c_str()  , 500000, O_RDWR | O_NOCTTY);
-        //         usleep(500000);
+        ClvHd::EMG_ADS1293Config config;
+        emg_pack.configure(config);
 
-        //         //setup the device (count and find the type of modules attached)
-        //         int n = device.setup();
+        emg_pack.start_acquisition();
+        std::cout << "EMG modules started" << std::fixed << std::setprecision(3);
 
-        //         //setup the emg modules
-        //         bool chx_enable[3] = {true,   // Enable channel 1
-        //                               false,  // Disable channel 2
-        //                               false}; // Disable channel 3
-        //         int route_table[3][2] = {
-        //             {1, 2},  // (-) and (+) electrodes of the first channel
-        //             {0, 1},  // (-) and (+) electrodes of the second channel
-        //             {0, 1}}; // (-) and (+) electrodes of the third channel
-        //         bool chx_high_res[3] = {
-        //             false, true, true}; // Enable or disable the high resolution mode
-        //         bool chx_high_freq[3] = {
-        //             false, true, true}; // Enable or disable the high frequency mode
-        //         int R1[3] = {2, 4, 4};  // Gain R1 of the INA channels
-        //         int R2 = 4;             // Gain R2 of the INA channels
-        //         int R3[3] = {4, 4, 4};  // Gain R3 of the INA channels
-        //         // Create and setup the EMG modules in the device
-        //         int nb_EMG_module =
-        //             ClvHd::EMG_ADS1293::setup(device, chx_enable, route_table,
-        //                                       chx_high_res, chx_high_freq, R1, R2, R3);
+        while(true)
+        {
+            std::vector<ClvHd::Value *> values = emg_pack.read_all();
+            double timestamp = values[0]->time_s + values[0]->time_ns / 1000000.0;
+            std::cout << "t: " << timestamp << " ";
+            for(size_t i = 0; i < emg_pack.modules.size(); i++)
+            {
+                std::cout << i << ": [";
+                for(int j = 0; j < values[i]->data.size(); j++)
+                {
+                    std::cout << std::setw(6) << std::setfill(' ') << 1000 * values[i]->data[j] << " "; 
+                    // values[i]->data[j] * 1000 << " ";
+                }
+                std::cout << "]\t";
+            }
+            std::cout << "\xd" << std::flush;
+            usleep(50000);
+        }
 
-        //         //return 0;
-
-        //         //start the emg modules
-        //         ClvHd::EMG_ADS1293::start_acquisition(device);
-
-        //         std::vector<double> sample_fast(nb_EMG_module * 3);
-        //         std::vector<double> sample_precise(nb_EMG_module * 3);
-        //         uint8_t flags[nb_EMG_module];
-        //         for(int t = 0;; t++)
-        //         {
-        //             uint64_t timestamp = ClvHd::EMG_ADS1293::read_all(
-        //                 device, sample_fast.data(), sample_precise.data(), flags);
-        //             std::cout << "timestamp: " << timestamp << " ";
-        //             for(int i = 0; i < nb_EMG_module; i++)
-        //             {
-        //                 for(int j = 0; j < 3; j++)
-        //                 {
-        //                     //check if the value is available
-        //                     if((flags[i] >> (2 + j)) & 0b1) // 5+j for precise value
-        //                         std::cout << std::fixed << std::setprecision(2)
-        //                                   << sample_fast[3 * i + j] * 1000 << " ";
-        //                     else // add NaN
-        //                         std::cout << "NaN ";
-        //                 }
-        //                 std::cout << std::fixed << std::setprecision(2)
-        //                           << sample_fast[i] * 1000 << " ";
-        //             }
-        //             std::cout << std::endl;
-        //             usleep(500000);
-        //         }
     }
     catch(std::exception &e)
     {
--- src/main_lsl.cpp
+++ src/main_lsl.cpp
@@ -1,79 +1,148 @@
-// #include <stdint.h>
-// #include <iomanip>
-// #include <iostream>
-// #include <stdio.h>
-// #include <stdlib.h>
 
-// #include <lsl_cpp.h>
+#include "clvHd.hpp"
+#include <lsl_cpp.h>
 
-// #include "clvHd.hpp"
+void
+usage(char *name)
+{
+    std::cerr << "Usage: " << name << " <serial_port>" << std::endl;
+}
 
 int
 main(int argc, char *argv[])
 {
-    // std::cout << "CleverHand Serial Interface:" << std::endl;
-    // try
-    // {
-    //     ClvHd::Device device(3);
-        
-    //     //open the serial connection between the computer and the controller board
-    //     device.controller.open_connection("/dev/tty.usbmodem145149601", 500000, O_RDWR | O_NOCTTY);
-    //     usleep(500000);
+    std::string port;
+    //default port
+#if defined(__linux__)
+    port = "/dev/ttyACM0";
+#elif defined(__APPLE__)
+    port = "/dev/tty.usbmodem145149601";
+#elif defined(_WIN32)
+    port = "COM3";
+#endif
 
-    //     //setup the controller board (count and find the type of modules attached)
-    //     device.controller.setup();
-        
-    //     //setup the emg modules
-    //     bool chx_enable[3] = {true, false, false}; // Enable or disable the channels
-    //     int route_table[3][2] = {{0, 1}, {0, 1}, {0, 1}};// Define the electrodes to use for each channel
-    //     bool chx_high_res[3] = {true, true, true}; // Enable or disable the high resolution mode
-    //     bool chx_high_freq[3] = {true, true, true}; // Enable or disable the high frequency mode
-    //     int R1[3] = {4, 4, 4}; // Gain R1 of the INA channels
-    //     int R2 = 4; // Gain R2 of the INA channels
-    //     int R3[3] = {4, 4, 4}; // Gain R3 of the INA channels
-    //     // Create and setup the EMG modules in the device
-    //     int nb_EMG_module = ClvHd::EMG_ADS1293::setup(device, chx_enable, route_table,  chx_high_res, chx_high_freq, R1, R2, R3);
-        
-    //     //start the emg modules
-    //     ClvHd::EMG_ADS1293::start_acquisition(device);
+    if(argc == 2)
+        port = argv[1];
 
-    //     int nb_ch = nb_EMG_module * 3;
-    //     lsl::stream_info info_sample("EMG", "sample_fast", nb_ch, 0,
-    //                                  lsl::cf_double64);
-    //     lsl::stream_outlet outlet_sample(info_sample);
-    //     std::vector<double> sample_fast(nb_ch);
-    //     std::vector<double> sample_precise(nb_ch);
-    //     uint8_t flags[nb_EMG_module];
-    //     std::cout << "[INFOS] Now sending data... " << std::endl;
-    //     for(int t = 0;; t++)
-    //     {
-    //         uint64_t timestamp = ClvHd::EMG_ADS1293::read_all(device, sample_fast.data(), sample_precise.data(), flags);
-    //         std::cout << "timestamp: " << timestamp << " ";
-    //         outlet_sample.push_sample(sample_fast, timestamp/1000000.0);
-    //     }
-    //     // for(int t = 0;; t++)
-    //     // {
-    //     //     uint64_t timestamp;
-    //     //     master.read_all_signal(&timestamp);
-    //     //     // std::cout << "timestamp: " << timestamp << " ";
-    //     //     // std::cout << (int)master.m_EMG[0]->get_regs()[ClvHd::DATA_STATUS_REG] << " 0b" << master.byte2bits(master.m_EMG[0]->get_regs()[ClvHd::DATA_STATUS_REG]) << std::endl;
-    //     //     for(int i = 0; i < 3; i++)
-    //     //     {
-    //     //         sample[i] = master.fast_EMG(0, i);
-    //     //         sample[i + 3] = master.precise_EMG(0, i);
-    //     //     }
-    //     //     outlet_sample.push_sample(sample, timestamp/1000000.0);
-    //     //     std::cout << "t: " << std::setw(9) << timestamp << ", EMG: " << std::setw(7) << sample[0] << " " << std::setw(7) << sample[1] << " " << std::setw(7) << sample[2] << " " << std::setw(7) << sample[3] << " " << std::setw(7) << sample[4] << " " << std::setw(7) << sample[5] << std::endl;
-    //     // }
-    // }
-    // catch(std::exception &e)
-    // {
-    //     std::cerr << "[ERROR] Got an exception: " << e.what() << std::endl;
-    // }
-    // catch(std::string str)
-    // {
-    //     std::cerr << "[ERROR] Got an exception: " << str << std::endl;
-    // }
+    try
+    {
+        std::cout << "CleverHand Serial Interfacee:" << std::endl;
+        ClvHd::Device device(3);
+        std::cout << "Opening serial port: " << port << std::endl;
+        device.initSerial(port.c_str());
+
+        ClvHd::EMG_ADS1293Pack emg_pack(&device, 3);
+        emg_pack.setup();
+
+        ClvHd::EMG_ADS1293Config config;
+        emg_pack.configure(config);
+
+        int nb_ch = emg_pack.modules.size() * 3;
+        lsl::stream_info info_sample("EMG", "sample_fast", nb_ch, 0,
+                                     lsl::cf_double64);
+        lsl::stream_outlet outlet_sample(info_sample);
+        std::vector<double> sample_fast(nb_ch);
+
+        emg_pack.start_acquisition();
+        std::cout << "EMG modules started" << std::endl;
+
+        std::cout << "[INFOS] Now sending data... " << std::endl;
+        while(true)
+        {
+            std::vector<ClvHd::Value *> values = emg_pack.read_all();
+            for(size_t i = 0; i < emg_pack.modules.size(); i++)
+            {
+                for(int j = 0; j < values[i]->data.size(); j++)
+                {
+                    sample_fast[3 * i + j] = values[i]->data[j] * 1000;
+                }
+            }
+            double timestamp = values[0]->time_s + values[0]->time_ns / 1000000.0;
+            std::cout << "timestamp: " << timestamp << "\t" << sample_fast[0]
+                      << "     \xd" << std::flush;
+            outlet_sample.push_sample(sample_fast, timestamp);
+            usleep(50000);
+        }
+    }
+    catch(std::exception &e)
+    {
+        std::cerr << "[ERROR] Got an exception: " << e.what() << std::endl;
+    }
+    catch(std::string str)
+    {
+        std::cerr << "[ERROR] Got an exception: " << str << std::endl;
+        usage(argv[0]);
+    }
 
     return 0; // success
 }
+
+// int
+// main(int argc, char *argv[])
+// {
+// std::cout << "CleverHand Serial Interface:" << std::endl;
+// try
+// {
+//     ClvHd::Device device(3);
+
+//     //open the serial connection between the computer and the controller board
+//     device.controller.open_connection("/dev/tty.usbmodem145149601", 500000, O_RDWR | O_NOCTTY);
+//     usleep(500000);
+
+//     //setup the controller board (count and find the type of modules attached)
+//     device.controller.setup();
+
+//     //setup the emg modules
+//     bool chx_enable[3] = {true, false, false}; // Enable or disable the channels
+//     int route_table[3][2] = {{0, 1}, {0, 1}, {0, 1}};// Define the electrodes to use for each channel
+//     bool chx_high_res[3] = {true, true, true}; // Enable or disable the high resolution mode
+//     bool chx_high_freq[3] = {true, true, true}; // Enable or disable the high frequency mode
+//     int R1[3] = {4, 4, 4}; // Gain R1 of the INA channels
+//     int R2 = 4; // Gain R2 of the INA channels
+//     int R3[3] = {4, 4, 4}; // Gain R3 of the INA channels
+//     // Create and setup the EMG modules in the device
+//     int nb_EMG_module = ClvHd::EMG_ADS1293::setup(device, chx_enable, route_table,  chx_high_res, chx_high_freq, R1, R2, R3);
+
+//     //start the emg modules
+//     ClvHd::EMG_ADS1293::start_acquisition(device);
+
+//     int nb_ch = nb_EMG_module * 3;
+//     lsl::stream_info info_sample("EMG", "sample_fast", nb_ch, 0,
+//                                  lsl::cf_double64);
+//     lsl::stream_outlet outlet_sample(info_sample);
+//     std::vector<double> sample_fast(nb_ch);
+//     std::vector<double> sample_precise(nb_ch);
+//     uint8_t flags[nb_EMG_module];
+//     std::cout << "[INFOS] Now sending data... " << std::endl;
+//     for(int t = 0;; t++)
+//     {
+//         uint64_t timestamp = ClvHd::EMG_ADS1293::read_all(device, sample_fast.data(), sample_precise.data(), flags);
+//         std::cout << "timestamp: " << timestamp << " ";
+//         outlet_sample.push_sample(sample_fast, timestamp/1000000.0);
+//     }
+//     // for(int t = 0;; t++)
+//     // {
+//     //     uint64_t timestamp;
+//     //     master.read_all_signal(&timestamp);
+//     //     // std::cout << "timestamp: " << timestamp << " ";
+//     //     // std::cout << (int)master.m_EMG[0]->get_regs()[ClvHd::DATA_STATUS_REG] << " 0b" << master.byte2bits(master.m_EMG[0]->get_regs()[ClvHd::DATA_STATUS_REG]) << std::endl;
+//     //     for(int i = 0; i < 3; i++)
+//     //     {
+//     //         sample[i] = master.fast_EMG(0, i);
+//     //         sample[i + 3] = master.precise_EMG(0, i);
+//     //     }
+//     //     outlet_sample.push_sample(sample, timestamp/1000000.0);
+//     //     std::cout << "t: " << std::setw(9) << timestamp << ", EMG: " << std::setw(7) << sample[0] << " " << std::setw(7) << sample[1] << " " << std::setw(7) << sample[2] << " " << std::setw(7) << sample[3] << " " << std::setw(7) << sample[4] << " " << std::setw(7) << sample[5] << std::endl;
+//     // }
+// }
+// catch(std::exception &e)
+// {
+//     std::cerr << "[ERROR] Got an exception: " << e.what() << std::endl;
+// }
+// catch(std::string str)
+// {
+//     std::cerr << "[ERROR] Got an exception: " << str << std::endl;
+// }
+
+//     return 0; // success
+// }
