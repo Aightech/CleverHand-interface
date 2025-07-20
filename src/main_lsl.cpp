28f61a89526705d9c5b12d24882d6d05fb0d9414
adev <long.devillard@gmail.com>
2025-07-20 23:30:25 +0900

    updated

--- src/main_lsl.cpp
+++ src/main_lsl.cpp
@@ -13,19 +13,33 @@ main(int argc, char *argv[])
         device.initSerial("/dev/ttyACM0", 500000, O_RDWR | O_NOCTTY);
         // device.initWifi();
 
-        ClvHd::EMG_ADS1293Pack emg_pack(&device, 3);
+        ClvHd::EMG_ADS1293Pack emg_pack(&device, 1);
         emg_pack.setup();
 
         ClvHd::EMG_ADS1293Config config;
-        config.route_table[0][0] = 5;
-        config.route_table[0][1] = 4;
-        config.route_table[1][0] = 0;
-        config.route_table[1][1] = 0;
-        config.route_table[2][0] = 0;
-        config.route_table[2][1] = 0;
+        config.route_table[0][0] = 1;
+        config.route_table[0][1] = 2;
+        config.route_table[1][0] = 3;
+        config.route_table[1][1] = 4;
+        config.route_table[2][0] = 5;
+        config.route_table[2][1] = 1;
         config.chx_enable[0] = true;
-        config.chx_enable[1] = false;
-        config.chx_enable[2] = false;
+        config.chx_enable[1] = true;
+        config.chx_enable[2] = true;
+        config.chx_high_res[0] = true;
+        config.chx_high_res[1] = true;
+        config.chx_high_res[2] = true;
+        config.chx_high_freq[0] = true;
+        config.chx_high_freq[1] = true;
+        config.chx_high_freq[2] = true;
+        config.set_R1(0, 2);
+        config.set_R1(1, 2);
+        config.set_R1(2, 2);
+        config.set_R2(4);
+        config.set_R3(0, 4);
+        config.set_R3(1, 4);
+        config.set_R3(2, 4);
+        config.set_clock_intern(true);
         emg_pack.configure(config);
 
         int nb_ch = emg_pack.modules.size() * 3;
@@ -41,7 +55,7 @@ main(int argc, char *argv[])
         std::cout << "[INFOS] Now sending data... " << std::endl;
         while(true)
         {
-            std::vector<ClvHd::Value *> values = emg_pack.read_all();
+            std::vector<ClvHd::Value *> values = emg_pack.read_all(false);
             for(size_t i = 0; i < emg_pack.modules.size(); i++)
             {
                 for(int j = 0; j < values[i]->data.size(); j++)
@@ -53,7 +67,7 @@ main(int argc, char *argv[])
             std::cout << "timestamp: " << timestamp << "\t" << sample_fast[0]
                       << "     \xd" << std::flush;
             outlet_sample.push_sample(sample_fast, timestamp);
-            usleep(1000);
+            usleep(500);
         }
     }
     catch(std::exception &e)
